0af5912b9b1f4e7dd45069ba374933a27e0129c9
Revelation <113563575+Pururutz@users.noreply.github.com>
2022-09-30 19:15:49 +0700

    Add files via upload

---
+++ Wahyu Setyanto Tugas Bilangan Genap.java
@@ -0,0 +1,15 @@
+﻿import java.util.*;
+import java.lang.Math;
+
+class JavaApplication {
+    public static void main(String[] args) {
+        int BilGen;
+        
+        System.out.println("Sebutkan 10 bilangan genap jika 0 tidak dihitung");
+        BilGen = 0;
+        while (BilGen < 20) {
+            BilGen = BilGen + 2;
+            System.out.println("10 bilangan genap adalah " + BilGen);
+        }
+    }
+}
---
+++ Wahyu Setyanto Tugas Jenis Kelamin.java
@@ -0,0 +1,23 @@
+﻿import java.util.*;
+import java.lang.Math;
+import java.util.Scanner;
+
+class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        String lakilaki, perempuan, gender;
+        
+        System.out.println("Masukan jenis kelamin anda");
+        gender = input.nextLine();
+        if (gender.equals("lakilaki")) {
+            System.out.println("Anda Laki-Laki");
+        } else {
+            if (gender.equals("perempuan")) {
+                System.out.println("Anda Perempuan");
+            } else {
+                System.out.println("Gender tidak diketahui");
+            }
+        }
+    }
+}
